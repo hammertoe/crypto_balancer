7b3cb653ec856e9b5f84508318a6f8ac969a70b0
Matt Hamilton <matt@enquos.com>
2019-02-23 13:57:21 +0000

    Remove dead code

--- crypto_balancer/order.py
+++ crypto_balancer/order.py
@@ -12,9 +12,6 @@ class Order():
     def __repr__(self):
         return "{} {} {}".format(self.direction, self.amount, self.pair)
 
-    def __cmp__(self, other):
-        return cmp(self.__dict__, other.__dict__)
-
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
