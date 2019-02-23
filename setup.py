8c0bcf646e754065b1ce82e84c68fc90c0bb4a51
Matt Hamilton <matt@enquos.com>
2019-02-23 00:50:49 +0000

    added travis config

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='crypto_balancer',
+      version='0.1',
+      packages=['crypto_balancer'],
+      entry_points={
+          'console_scripts': [
+              'crypto_balancer = crypto_balancer.crypto_balancer:main'
+          ]
+      },
+      )
