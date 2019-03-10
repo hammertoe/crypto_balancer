76ac3ef710f4018638536d6154e308d36fff767e
Matt Hamilton <mh@quernus.co.uk>
2019-03-10 11:57:22 +0100

    version bump, add example config file, and prepare for pypi upload

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 from setuptools import setup
 
 setup(name='crypto_balancer',
-      version='0.1',
+      version='3.1',
       packages=['crypto_balancer'],
       entry_points={
           'console_scripts': [
               'crypto_balancer = crypto_balancer.main:main'
           ]
       },
+      license='MIT',
+      description = 'A tool to automatically balance cryptocurrency portfolios',
+      author = 'Matt Hamilton',
+      author_email = 'mh@quernus.co.uk',
+      url = 'https://github.com/hammertoe/crypto_balancer',
+      download_url = 'https://github.com/hammertoe/crypto_balancer/archive/3.1.tar.gz',
+      keywords = ['cryptocurrency', 'portfolio', 'xrp', 'ethereum', 'bitcoin', 'btc', 'eth'],
+      install_requires=[
+          'ccxt',
+      ],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Topic :: Office/Business :: Financial :: Investment',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+      ],
       )
